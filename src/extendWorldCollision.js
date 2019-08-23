324f0cb77c8d6a5abd97401b0c67e79ad0c6de98
pentamania <tanigami7220@gmail.com>
2019-08-23 17:43:57 +0900

    [update] modify and complete world-collision-extension methods

--- src/extendWorldCollision.js
+++ src/extendWorldCollision.js
@@ -1,54 +1,84 @@
 /**
  * Enables phina.app.Object2D inherited Class the methods related to world-coordinate postion and collision.
- * exends phina.app.Object2D.prototype
- * WIP
+ * TODO: test methods
  */
+export var EXTENDED_METHODS = [
+  // /**
+  //  * template
+  //  */
+  // {
+  //   name: "",
+  //   func: function() {
+  //   }
+  // },
 
-var EXTENDED_METHODS = [
   /**
    * phina.app.Object2D.prototype.getWorldPosition
-   * @param {boolean} useCache - ワールド座標の再計算をしない
+   * @param {boolean} useCache - ワールド座標の再計算をせず、計算済みの値を使うかどうか
    * @return {phina.geom.Vector2} 自身のワールド座標を返す
    */
   {
     name: "getWorldPosition",
     func: function(useCache=false) {
-      var wm = (useCache) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
+      var wm = (useCache === true) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
       return phina.geom.Vector2(wm.m02, wm.m12);
     }
   },
 
   /**
-   * [getWorldCircle description]
-   * @return {phina.geom.Circle} 自身のワールド円形判定を返す
+   * phina.app.Object2D.prototype.getWorldCircle
+   * @param {boolean} useCache - ワールド座標の再計算をせず、計算済みの値を使うかどうか
+   * @return {phina.geom.Circle} 自身のワールド座標上の円形判定を返す
    */
   {
     name: "getWorldCircle",
     func: function(useCache=false) {
-      var wm = (useCache) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
+      var wm = (useCache === true) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
       return phina.geom.Circle(wm.m02, wm.m12, this.radius);
     }
   },
 
   /**
-   * [getWorldRect description]
+   * phina.app.Object2D.prototype.getWorldRect
+   * @param {boolean} useCache - ワールド座標の再計算をせず、計算済みの値を使うかどうか
    * @return {phina.geom.Rect} 自身のワールド座標上の矩形判定を返す
    */
   {
     name: "getWorldRect",
     func: function(useCache=false) {
-      var wm = (useCache) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
-      return phina.geom.Rect(wm.m02, wm.m12, this.width, this.height);
+      var wm = (useCache === true) ? this._worldMatrix : this._calcWorldMatrix()._worldMatrix;
+      const left = wm.m02 - this.origin.x * this.width;
+      const top = wm.m12 - this.origin.y * this.height;
+      return phina.geom.Rect(left, top, this.width, this.height);
     }
   },
 
   /**
-   * needs tests
-   * @return {boolean}
+   * phina.app.Object2D.prototype.hitTestWorldPoint
+   * @return {boolean} hittest result
    */
   {
-    name: "hitTestWorld",
-    func: function(useCache=false) {
+    name: "hitTestWorldPoint",
+    func: function(target) {
+      let hitArea;
+      if (this.boundingType === 'rect') {
+        hitArea = this.getWorldRect();
+      } else {
+        hitArea = this.getWorldCircle();
+      }
+      return hitArea.contains(target.wx, target.wy);
+    }
+  },
+
+  /**
+   * phina.app.Object2D.prototype.hitTestWorldTarget
+   * returns hittest using phina.geom.Collision
+   * depends on boundingType
+   * @return {boolean} hittest result
+   */
+  {
+    name: "hitTestWorldTarget",
+    func: function(target, useCache=true) {
       if (this.boundingType === 'rect') {
         // rect vs ...
         const thisHitRect = this.getWorldRect(useCache);
@@ -60,12 +90,12 @@ var EXTENDED_METHODS = [
             return phina.geom.Collision.testCircleRect(target.getWorldCircle(useCache), thisHitRect);
             break;
         }
-      } else {
+      } else if (this.boundingType === 'circle') {
         // circle vs ...
         const thisHitCircle = this.getWorldCircle(useCache);
         switch (target.boundingType) {
           case 'rect':
-            return phina.geom.Collision.testCircleRect(target.getWorldRect(useCache), thisHitCircle);
+            return phina.geom.Collision.testCircleRect(thisHitCircle, target.getWorldRect(useCache));
             break;
           case 'circle':
             return phina.geom.Collision.testCircleCircle(thisHitCircle, target.getWorldCircle(useCache))
@@ -74,6 +104,31 @@ var EXTENDED_METHODS = [
       }
     }
   },
+
+  /**
+   * phina.app.Object2D.prototype.hitTestRecursive
+   * targetのchildrenまで下ってそれぞれコールバック処理を行う
+   * 深さ優先探索
+   * @params {function} success
+   * @params {function} failure
+   * @return {void}
+   */
+  {
+    name: "hitTestRecursive",
+    func: function(target, success, failure) {
+      if (this.hitTestWorldTarget(target)) {
+        success(target);
+      } else {
+        if (failure && typeof failure === 'function') failure(target);
+      }
+      if (target.children && target.children.length) {
+        target.children.forEach((child)=> {
+          this.hitTestRecursive(child, success, failure);
+        });
+      }
+    }
+  },
+
 ];
 
 // phina.app.Object2D.prototype.getWorldPosition = function() {
@@ -123,5 +178,4 @@ export default function() {
   phina.app.Object2D.prototype.getter("wy", function() {
     return this._worldMatrix.m12;
   });
-
 }
